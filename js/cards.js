32d712f14cde44587abe8104b3819c75c3eb44fc
千叶 <60874481+qhongyi123@users.noreply.github.com>
2026-05-22 18:50:58 +0800

    Add files via upload

--- js/cards.js
+++ js/cards.js
@@ -3,18 +3,10 @@ var START_CARDS_FILES = [
   'data/starts/s002-面对大街上随意撸管的痴女大姐姐，彩花：任何邪恶都将被绳之以法！.json',
   'data/starts/s003-地铁之狼现身！小巡防官危矣！.json',
   'data/starts/s004-全民偶像的生日会？琉璃选妃罢了！.json',
-  'data/starts/s005-边界协议.json',
-  'data/starts/s006-园丁一天.json',
-  'data/starts/s007-夜色花房.json',
-  'data/starts/s008-扩建计划.json',
-  'data/starts/s009-次领域展开.json',
   'data/starts/s0067-闲着没事，和女儿来一炮.json'
 ];
 
 var SUBMISSION_CARDS_FILES = [
-  'data/submissions/sub001-新芽.json',
-  'data/submissions/sub002-教会见闻.json',
-  'data/submissions/sub003-影子一夜.json',
   'data/submissions/sub0013-扮演焰的妻子，玩视觉剥夺.json',
   'data/submissions/sub0035-枯萎教会大侵攻！跟我一起守营！.json',
   'data/submissions/sub0036-枯萎教会大侵攻！是雏菊就防守一百天！.json',
@@ -25,16 +17,10 @@ var SUBMISSION_CARDS_FILES = [
 ];
 
 var CHARACTER_CARDS_FILES = [
-  'data/characters/神崎澪.json',
   'data/characters/长风.json',
-  'data/characters/白雪.json',
-  'data/characters/琉璃.json',
   'data/characters/爱尔温.json'
 ];
 
 var MOD_CARDS_FILES = [
-  'data/mods/mod001-花园主题包.json',
-  'data/mods/mod002-快捷指令集.json',
-  'data/mods/mod003-骰子扩展.json',
   'data/mods/mod0067-彩花的妈妈.json'
 ];
