4b22c3663207db99df8bb6b0f9c282e05bfe8ece
千叶 <60874481+qhongyi123@users.noreply.github.com>
2026-05-22 17:54:36 +0800

    Add files via upload

--- js/cards.js
+++ js/cards.js
@@ -20,14 +20,16 @@ var SUBMISSION_CARDS_FILES = [
   'data/submissions/sub0036-枯萎教会大侵攻！是雏菊就防守一百天！.json',
   'data/submissions/sub0037-开局继承百亿和佣兵公司，登上影子帝王的宝座吧！.json',
   'data/submissions/sub0038-东京陷落！雏菊沉没之日！？.json',
-  'data/submissions/sub0042-黄毛の薬物侵害：琉璃无惨.json'
+  'data/submissions/sub0042-黄毛の薬物侵害：琉璃无惨.json',
+  'data/submissions/sub0043-影子的友谊.json'
 ];
 
 var CHARACTER_CARDS_FILES = [
   'data/characters/神崎澪.json',
   'data/characters/长风.json',
   'data/characters/白雪.json',
-  'data/characters/琉璃.json'
+  'data/characters/琉璃.json',
+  'data/characters/爱尔温.json'
 ];
 
 var MOD_CARDS_FILES = [
